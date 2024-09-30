1a0a8261db2ef76dd34b5fca5f31a379cb661122
Carloss <131725121+pedroh73737373@users.noreply.github.com>
2024-09-30 15:44:58 +0000

    aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+export default function Index() {
 
-function Index(){
-    return 
+    return <>
 
-    <div> 
-        <Navbar />
-        <h1>Olá Mundo</h1>
-        <Footer/>
-    </div>
-    
+        <div>
+            <Navbar />
+            <Container>
+                <Row>
+                    <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
+            <Footer />
+        </div>
+    </>;
 }
-export default Index;
